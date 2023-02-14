77367d0f5d310df9a48fb67a3d9fd2e921297c94
Jun Kim <jk9eem@virginia.edu>
2023-02-13 22:06:11 -0500

    deleting app-header div

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        // <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +16,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
     </div>
   );
 }
