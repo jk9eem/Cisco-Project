79dbf0007a37ba1750e2d3d0f79ac35c93c8e991
Jun Kim <jk9eem@virginia.edu>
2023-02-15 23:45:20 -0500

    edit exhibit contents

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -15,31 +15,19 @@ function App() {
           <div class="exhibit-content">
             <img
               src="https://cdn.cookielaw.org/logos/03fc55fe-0057-4b2f-817d-763e7ecdb316/ad0fee7c-eecb-4e6d-b90f-8b6ca3a1e95c/cisco-logo-transparent.png"
-              alt="cisco logo imange"
+              alt="cisco logo image"
             ></img>
           </div>
+        
 
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
+          <ul class="list-content">
+            <li>Security everywhere for hybrid work anywhere</li>
+            <li>Implement cloud your way</li>
+            <li>Protect users, devices, and your network</li>
           </ul>
-
-          <table>
-            <tr>
-              <th>Column 1</th>
-              <th>Column 2</th>
-            </tr>
-            <tr>
-              <td>Row 1, Column 1</td>
-              <td>Row 1, Column 2</td>
-            </tr>
-            <tr>
-              <td>Row 2, Column 1</td>
-              <td>Row 2, Column 2</td>
-            </tr>
-          </table>
-        </div>
+          
+        </div>    
+        
       </main>
       <p>
         Edit <code>src/App.js</code> and save to reload.
